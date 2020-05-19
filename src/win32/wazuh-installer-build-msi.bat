1b3ae44f1383ed23b3cb18337a57d9a6518bab31
Sashank <sashank@sashank.co>
2020-05-19 07:55:23 +0530

    service rename in installer

--- src/win32/wazuh-installer-build-msi.bat
+++ src/win32/wazuh-installer-build-msi.bat
@@ -6,10 +6,10 @@ set VERSION=%1
 set REVISION=%2
 
 REM IF VERSION or REVISION are empty, ask for their value
-IF [%VERSION%] == [] set /p VERSION=Enter the version of the Wazuh agent (x.y.z):
-IF [%REVISION%] == [] set /p REVISION=Enter the revision of the Wazuh agent:
+IF [%VERSION%] == [] set /p VERSION=Enter the version of the Seconize agent (x.y.z):
+IF [%REVISION%] == [] set /p REVISION=Enter the revision of the Seconize agent:
 
-SET MSI_NAME=wazuh-agent-%VERSION%-%REVISION%.msi
+SET MSI_NAME=seconize-agent-%VERSION%-%REVISION%.msi
 
 candle.exe -nologo "wazuh-installer.wxs" -out "wazuh-installer.wixobj" -ext WixUtilExtension -ext WixUiExtension
 light.exe "wazuh-installer.wixobj" -out "%MSI_NAME%"  -ext WixUtilExtension -ext WixUiExtension
